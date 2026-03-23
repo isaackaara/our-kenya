28b220d4a40b8684f4e60bc1737999d4b83db8ed
Isaac Hunja <isaac@kaara.works>
2026-03-23 13:09:20 +0300

    Update loading message: "can take up to 30 seconds"

--- quartz/components/ListenButton.tsx
+++ quartz/components/ListenButton.tsx
@@ -41,7 +41,7 @@ const ListenButton: QuartzComponent = () => {
         >
           <path d="M12 2v4M12 18v4M4.93 4.93l2.83 2.83M16.24 16.24l2.83 2.83M2 12h4M18 12h4M4.93 19.07l2.83-2.83M16.24 7.76l2.83-2.83" />
         </svg>
-        <span class="ok-loading-text">Generating audio<br/>This typically takes 20–30 seconds</span>
+        <span class="ok-loading-text">Generating audio<br/>This can take up to 30 seconds</span>
       </div>
 
       {/* Expanded player controls (hidden until audio loads) */}
